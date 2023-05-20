98cd4fa5a3f39a49ea8d894c4f1d121ed183319c
honggibeom <90910169+honggibeom@users.noreply.github.com>
2023-05-20 17:23:09 +0900

    Update MemberController.java

--- src/main/java/jpabook/springjpashop/controller/MemberController.java
+++ src/main/java/jpabook/springjpashop/controller/MemberController.java
@@ -1,14 +1,20 @@
 package jpabook.springjpashop.controller;
 
 
+import jpabook.springjpashop.Entity.MakeSentence.MakeSentenceEntity;
 import jpabook.springjpashop.Entity.MemberEntity;
+import jpabook.springjpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
-//@RequestMapping("/api/spring")
+@RequiredArgsConstructor
 public class MemberController {
 
+    private final MemberRepository memberRepository;
 
     @GetMapping("/uerInpo")
     public String getMember(@AuthenticationPrincipal MemberEntity memberEntity){
@@ -16,7 +22,14 @@ public class MemberController {
         return "로그인된 사용자는"+ memberEntity.getUserId() + "입니다";
     }
 
+    @GetMapping("/api/auth/user/{id}")
+    public MemberEntity getUser(@PathVariable Long id){
 
+        MemberEntity result = this.memberRepository.findOne(id);
+
+        return result;
+
+    }
 
 //    @GetMapping("/members/new")
 //    public String createForm(Model model) {
